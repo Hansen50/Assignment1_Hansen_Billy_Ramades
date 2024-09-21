086758f4de3024e9ef1cce4ea04f9258f9f03d2b
Hansen <hansenbilly5@gmail.com>
2024-09-22 00:34:19 +0700

    Latest improve code

--- app/src/main/java/com/example/assignment1_hansenbillyramades/TransactionHistoryActivity.kt
+++ app/src/main/java/com/example/assignment1_hansenbillyramades/TransactionHistoryActivity.kt
@@ -5,13 +5,29 @@ class TransactionHistoryActivity(private val financeManager: FinanceManager) {
     fun showTransactionHistory() {
         println("Transaction History")
         val history = financeManager.getTransactionHistory()
+        val incomeTransactions = history.filter { it.type == TransactionType.INCOME }
+        val expenseTransactions = history.filter { it.type == TransactionType.EXPENSE }
         if (history.isEmpty()) {
             println("No transactions recorded")
-        } else {
-            for (transaction in history) {
-                println("========||======")
+        }
+            if (incomeTransactions.isNotEmpty()) {
+                println("Income Transactions:")
+                for (transaction in incomeTransactions) {
+                    println("${transaction.type} ${transaction.amount}")
+                }
+            } else {
+                println("No Transactions Income")
+            }
+
+        println("=====================")
+
+        if (expenseTransactions.isNotEmpty()) {
+            println("Expense Transactions:")
+            for (transaction in expenseTransactions) {
                 println("${transaction.type} ${transaction.amount}")
             }
+        } else {
+            println("No Transactions Expense")
+        }
         }
     }
-}
